df5fd08bdc80b979f4513662decb6aeb250bb5c5
gahyun1113 <tlsrkgus1113@gmail.com>
2024-10-22 22:30:53 +0900

    Feat: 배치 테스트 구조화

---
+++ src/main/java/com/example/sns_project/batch/repository/TestRepository.java
@@ -0,0 +1,4 @@
+package com.example.sns_project.batch.repository;
+
+public class TestRepository {
+}
---
+++ src/main/java/com/example/sns_project/batch/schedule/TestSchedule.java
@@ -0,0 +1,4 @@
+package com.example.sns_project.batch.schedule;
+
+public class TestSchedule {
+}

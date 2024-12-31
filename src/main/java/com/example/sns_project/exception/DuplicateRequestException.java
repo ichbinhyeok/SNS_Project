5e3f6de4dbab502e229366ad9e9a6f4b0e26dcb2
shinhyeok <shinhyeok22@gmail.com>
2024-12-31 15:46:06 +0900

    feat : 예외사항 클래스 추가

---
+++ src/main/java/com/example/sns_project/exception/DuplicateRequestException.java
@@ -0,0 +1,11 @@
+package com.example.sns_project.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)  // 409 Conflict
+public class DuplicateRequestException extends RuntimeException {
+    public DuplicateRequestException(String message) {
+        super(message);
+    }
+}
